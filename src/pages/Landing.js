ac141628378835309234399e134c7d54f444afd8
Javier Delgado <javierdelgado@Javiers-MacBook-Pro.local>
2022-05-29 15:53:08 -0700

    adds styled components and images to landing page

---
+++ src/pages/Landing.js
@@ -0,0 +1,28 @@
+import logo from "../assets/images/logo.svg";
+import main from "../assets/images/main.svg";
+import Wrapper from "../assets/wrappers/LandingPage";
+
+const Landing = () => {
+  return (
+      <Wrapper>
+        <nav>
+          <img src={logo} alt="jobify" className="logo"/>
+        </nav>
+          <div className="container page">
+            <div className="info">
+              <h1>job <span>tracking</span> app</h1>
+              <p>
+              I'm baby schlitz hella mustache brunch banjo prism. Flexitarian yuccie vice iPhone. Mustache jean shorts glossier prism locavore +1 raw denim, mixtape yes plz bitters. Single-origin coffee cred DIY, XOXO squid before they sold out mumblecore aesthetic. Microdosing taxidermy occupy you probably haven't heard of them readymade normcore. Vexillologist ugh asymmetrical, single-origin coffee vaporware la croix sartorial leggings VHS bitters praxis.
+              </p>
+              <button className="btn btn-hero">
+                Login/Register
+              </button>
+            </div>
+            <img src={main} alt="job hunt" className="img main-img" />
+          </div>
+      </Wrapper>
+   
+  )
+}
+
+export default Landing
