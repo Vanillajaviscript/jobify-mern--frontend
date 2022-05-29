7ab579e01c821e57c443ab77002c14bab05ecd35
Javier Delgado <javierdelgado@Javiers-MacBook-Pro.local>
2022-05-29 16:22:02 -0700

    adds routers and links

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import Landing from './pages/Landing';
 import styled from "styled-components";
-
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 
 
 function App() {
   return (
-    <div>
-    <Landing />
-    </div>
+    <Router>
+      <nav>
+        <Link to="/">Dashboard</Link>
+        <Link to="/register">Register</Link>
+        <Link to="/landing">Landing</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<div>Dashboard</div>}/>
+        <Route path="/register" element={<div>Register</div>}/>
+        <Route path="/landing" element={<Landing />}/>
+        <Route path="*" element={<h1>Error</h1>}/>
+      </Routes>
+    </Router>
   );
 }
 
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,12 +1,14 @@
 import logo from "../assets/images/logo.svg";
 import main from "../assets/images/main.svg";
 import Wrapper from "../assets/wrappers/LandingPage";
+import { Logo } from "../components/index.js";
+import {Link} from "react-router-dom";
 
 const Landing = () => {
   return (
       <Wrapper>
         <nav>
-          <img src={logo} alt="jobify" className="logo"/>
+          <Logo />
         </nav>
           <div className="container page">
             <div className="info">
@@ -14,9 +16,11 @@ const Landing = () => {
               <p>
               I'm baby schlitz hella mustache brunch banjo prism. Flexitarian yuccie vice iPhone. Mustache jean shorts glossier prism locavore +1 raw denim, mixtape yes plz bitters. Single-origin coffee cred DIY, XOXO squid before they sold out mumblecore aesthetic. Microdosing taxidermy occupy you probably haven't heard of them readymade normcore. Vexillologist ugh asymmetrical, single-origin coffee vaporware la croix sartorial leggings VHS bitters praxis.
               </p>
+              <Link to="/register">
               <button className="btn btn-hero">
                 Login/Register
               </button>
+              </Link>
             </div>
             <img src={main} alt="job hunt" className="img main-img" />
           </div>
