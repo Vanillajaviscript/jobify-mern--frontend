f9e567375eec322b7b40acc25a3ecc9ae7d68579
Javier Delgado <javierdelgado@Javiers-MacBook-Pro.local>
2022-05-30 15:58:30 -0700

    adds more hooks to register page

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import "normalize.css";
 import "./index.css";
-
+import { AppProvider } from './context/appContext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <App />
+      <AppProvider>
+        <App />
+      </AppProvider>
   </React.StrictMode>
 );
